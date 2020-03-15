eedc22c240815a7acd9e5a4b3bbc10e3100c1dbc
davisaaron <davisabraham32@gmail.com>
2020-03-15 17:42:05 +0000

    project

--- app/src/main/java/com/example/newone/MainActivity.java
+++ app/src/main/java/com/example/newone/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.newone;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button Login;
+    Button Register;
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = findViewById(R.id.uname);
+        password = findViewById(R.id.pwd);
+        Login = findViewById(R.id.login);
+        Register = findViewById(R.id.register);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("name", username.getText().toString());
+                intent.putExtra("name", password.getText().toString());
+                startActivity(intent);
+
+            }
+        });
     }
-}
+
+
+
+
+
+    }
+
+
+
+
