033119e382ab3996357afefa2a44b726df99ef61
davisaaron <davisabraham32@gmail.com>
2020-03-19 06:07:39 +0000

    erroes
    Signed-off-by: davisaaron <davisabraham32@gmail.com>

--- app/src/main/java/com/example/newone/parking.java
+++ app/src/main/java/com/example/newone/parking.java
@@ -40,7 +40,7 @@ public class parking extends AppCompatActivity {
         Edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(parking.this,HOME.class);
+                Intent intent = new Intent(parking.this,EditParking.class);
                 startActivity(intent);
             }
         });
@@ -48,7 +48,7 @@ public class parking extends AppCompatActivity {
         Payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(parking.this,Payment.class);
+                Intent intent = new Intent(parking.this,payment_recipt.class);
                 startActivity(intent);
             }
         });
