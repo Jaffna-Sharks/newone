9df1d2a9a3736e17fe718072bb80e0f39cd0a8b3
Aaron Davis <davisabraham32@gmail.com>
2020-04-17 20:08:41 +0530

    ad

---
+++ app/src/main/java/com/example/newone/Register.java
@@ -0,0 +1,72 @@
+package com.example.newone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    EditText rusername;
+    EditText rpassword;
+    EditText rconfrom_password;
+    Button Register;
+    Button Login;
+
+
+    LoginDatabaseHelper loginDatabaseHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        loginDatabaseHelper = new LoginDatabaseHelper(this);
+        rusername = findViewById(R.id.editText);
+        rpassword = findViewById(R.id.editText3);
+        rconfrom_password = findViewById(R.id.editText4);
+        Register = findViewById(R.id.register2);
+        Login = findViewById(R.id.login2);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(com.example.newone.Register.this, login.class);
+                startActivity(intent);
+            }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = rusername.getText().toString();
+                String password = rpassword.getText().toString();
+                String confirm_password = rconfrom_password.getText().toString();
+
+                if(username.equals("") || password.equals("") || confirm_password.equals("")){
+                    Toast.makeText(getApplicationContext(), "Fields Required", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(password.equals(confirm_password)){
+                        Boolean checkusername = loginDatabaseHelper.CheckUsername(username);
+                        if(checkusername == true){
+                            Boolean insert = loginDatabaseHelper.Insert(username, password);
+                            if(insert == true){
+                                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT).show();
+                                rusername.setText("");
+                                rpassword.setText("");
+                                rconfrom_password.setText("");
+                            }
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Username already taken", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Password does not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+   }
+}
--- app/src/main/java/com/example/newone/login.java
+++ app/src/main/java/com/example/newone/login.java
@@ -7,39 +7,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
 
-    EditText username;
-    EditText password;
+    EditText lusername;
+    EditText lpassword;
     Button Login;
     Button Register;
 
 
+    LoginDatabaseHelper loginDatabaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
-        username = findViewById(R.id.uname);
-        password = findViewById(R.id.pwd);
+
+        loginDatabaseHelper = new LoginDatabaseHelper(this);
+        lusername = findViewById(R.id.uname);
+        lpassword = findViewById(R.id.pwd);
         Login = findViewById(R.id.login);
         Register = findViewById(R.id.register);
 
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this,HOME.class);
-                startActivity(intent);
-            }
-        });
+
 
         Register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(login.this, Main2Activity.class);
+                Intent intent = new Intent(login.this, com.example.newone.Register.class);
 
                 startActivity(intent);
 
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = lusername.getText().toString();
+                String password = lpassword.getText().toString();
+
+                Boolean checklogin = loginDatabaseHelper.CheckLogin(username, password);
+                if(checklogin == true){
+                    Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(login.this, HOME.class);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
