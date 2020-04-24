2e2a28969fb1363bea8749387ab76de0666b79e7
Kaarun98 <kaarunmahi98@gmail.com>
2020-04-24 19:12:46 +0530

    edit home

--- app/src/main/java/com/example/newone/parking.java
+++ app/src/main/java/com/example/newone/parking.java
@@ -66,7 +66,13 @@ public class parking extends AppCompatActivity {
             }
         });
 
-        
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(parking.this,HOME.class);
+                startActivity(intent);
+            }
+        });
     }
     public void deleteData () {
         DELETE.setOnClickListener(
